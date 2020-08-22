90aaa6dec141f5d7a36edd75943ee06f991a201e
Lizkes <erfan2333@gmail.com>
2020-08-22 21:50:21 +0800

    change default

--- app/env.py
+++ app/env.py
@@ -6,15 +6,15 @@ config = {
     "remove_source": getenv("vconvert_remove_source", "false"),
     "remove_subtitle": getenv("vconvert_remove_subtitle", "false"),
     # mp4|mkv|webm
-    "format": getenv("vconvert_format", "webm"),
+    "format": getenv("vconvert_format", "mp4"),
     # 8|10
     # don't support vp9
     "bit": getenv("vconvert_bit", "8"),
     # h264|h265|vp9
-    "vc": getenv("vconvert_vc", "vp9"),
+    "vc": getenv("vconvert_vc", "h264"),
     # aac|opus
     # aac don't support webm
-    "ac": getenv("vconvert_ac", "opus"),
+    "ac": getenv("vconvert_ac", "aac"),
     "input": getenv("vconvert_input", "/vconvert_input"),
     "temp": getenv("vconvert_temp", "/vconvert_temp"),
 }
